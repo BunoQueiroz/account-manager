189e9229411a040f84cd10ddba480927b511ee36
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-09-27 07:20:28 -0300

    Alterando o tipo de 'price' do modelo de 'product'

--- apps/product/models/product.py
+++ apps/product/models/product.py
@@ -12,7 +12,7 @@ class Category(models.Model):
 class Product(models.Model):
 
     name = models.CharField(max_length=70)
-    price = models.DecimalField(max_digits=7, decimal_places=2)
+    price = models.FloatField()
     criation_date = models.DateField(auto_now=True)
     category = models.ForeignKey(Category, models.DO_NOTHING)
     description = models.CharField(max_length=200)

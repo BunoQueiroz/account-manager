78e9fdbcb47f88c987448225702a57d4da76d4fc
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-09-27 15:13:52 -0300

    Personalizando o site admin do django

--- apps/client/admin.py
+++ apps/client/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from client.models import Client, Account, Purchase
+from django.contrib.auth.models import User, Group
 
+# Admin Classes
 
 class ClientAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'cpf', 'email', 'birthday']
@@ -21,6 +23,28 @@ class PurchaseAdmin(admin.ModelAdmin):
     list_filter = ['account']
 
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username','first_name', 'last_name', 'email', 'is_superuser']
+    list_filter = ['is_superuser']
+    search_fields = ['first_name', 'last_name']
+    ordering = ['first_name']
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    search_fields = ['name']
+
+
+class Manager(admin.AdminSite):
+    site_header = 'Administration'
+    
+
+admin.site = Manager()
+
+# Admin Register
+
 admin.site.register(Client, ClientAdmin)
 admin.site.register(Account, AccountAdmin)
 admin.site.register(Purchase, PurchaseAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Group, GroupAdmin)

2e1da4421843791938cc1829cb56ed7e46403b1a
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-09-27 08:43:18 -0300

    Sobrescrevendo os métodos str das classes 'client' e 'account'

--- apps/client/models/account.py
+++ apps/client/models/account.py
@@ -6,3 +6,6 @@ class Account(models.Model):
 
     client = models.ForeignKey(Client, models.CASCADE)
     opened = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return f'account - {self.client.first_name}'
--- apps/client/models/client.py
+++ apps/client/models/client.py
@@ -8,3 +8,9 @@ class Client(models.Model):
     cpf = models.CharField(max_length=14, blank=True)
     email = models.EmailField(blank=True)
     birthday = models.DateField()
+
+    def __str__(self) -> str:
+        if self.last_name:
+            return f'{self.first_name} {self.last_name}'
+        return self.first_name
+    

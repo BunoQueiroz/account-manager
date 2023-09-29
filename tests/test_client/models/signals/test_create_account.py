8e4530c95e34007a5abd9a4cf66d7108280f04d5
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-09-29 08:07:54 -0300

    Desenvolvendo teste para criar uma conta sempre que um cliente for adicionado ao banco de dados

---
+++ tests/test_client/models/signals/test_create_account.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from client.models import Client, Account
+from datetime import datetime
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class CreateAccountTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.admin = User.objects.create_superuser('Bruno', password='MyPassword')
+        self.client.login(username='Bruno', password='MyPassword')
+
+        self.path = reverse('admin:client_client_add')
+        self.data = {
+            'first_name': 'Bruno',
+            'birthday': datetime.date(datetime.now())
+        }
+
+    def test_create_account_when_insert_new_client(self):
+        self.client.post(self.path, self.data)
+        client_test = Client.objects.get(first_name='Bruno')
+        account = Account.objects.filter(client=client_test).exists()
+        self.assertTrue(account)

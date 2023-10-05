9209cf8bc46c6a19fb7ab36929f7eee76a7f2728
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-10-05 10:34:50 -0300

    Corrigindo teste de atributos de 'Category'

--- tests/test_product/models/atributes/test_category_atributes.py
+++ tests/test_product/models/atributes/test_category_atributes.py
@@ -7,7 +7,7 @@ class CategoryAtributesTestCase(TestCase):
     def setUp(self) -> None:
         self.category = Category(name='Frios')
         self.category.save()
-        self.category_in_db = Category.objects.get(id=1)
+        self.category_in_db = Category.objects.get(name='Frios')
 
     def test_atributes_category_class(self):
         self.assertEqual(self.category.name, 'Frios')

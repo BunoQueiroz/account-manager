dc5866724925b1a7d0045470163e6ccc8bdd8c71
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-10-26 14:25:29 -0300

    Retornando o status code de 'page not founds'

--- apps/client/views.py
+++ apps/client/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def handler404(request, exception):
-    return render(request, 'core/page_not_found.html')
+    return render(request, 'core/page_not_found.html', status=404)

fde2345c5b2ac356142786ee72de8333577657ac
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-11-14 11:15:58 -0300

    Validando cpf de clientes (em caso de serem inseridos)

--- apps/client/forms/client_form.py
+++ apps/client/forms/client_form.py
@@ -13,10 +13,12 @@ class ClientForm(ModelForm):
         birthday = self.cleaned_data.get('birthday')
         first_name = self.cleaned_data.get('first_name')
         last_name = self.cleaned_data.get('last_name')
+        cpf = self.cleaned_data.get('cpf')
         errors_list = {}
         min_age(birthday, 18, errors_list)
         first_name_validator(first_name, errors_list)
         last_name_validator(last_name, errors_list)
+        cpf_validator(cpf, errors_list)
         if errors_list:
             for error in errors_list:
                 error_message = errors_list[error]

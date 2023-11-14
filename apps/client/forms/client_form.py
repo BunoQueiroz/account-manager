cebab64aca720331248c986269333005887f9555
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-11-14 10:23:23 -0300

    Validação de nome e sobrenome no formulário de clientes

--- apps/client/forms/client_form.py
+++ apps/client/forms/client_form.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm
 from client.models import Client
-from .validators import min_age
+from .validators import *
 
 
 class ClientForm(ModelForm):
@@ -11,8 +11,12 @@ class ClientForm(ModelForm):
 
     def clean(self):
         birthday = self.cleaned_data.get('birthday')
+        first_name = self.cleaned_data.get('first_name')
+        last_name = self.cleaned_data.get('last_name')
         errors_list = {}
         min_age(birthday, 18, errors_list)
+        first_name_validator(first_name, errors_list)
+        last_name_validator(last_name, errors_list)
         if errors_list:
             for error in errors_list:
                 error_message = errors_list[error]

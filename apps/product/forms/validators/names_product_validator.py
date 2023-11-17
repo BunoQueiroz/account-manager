2595331358b2f6dec787ccc5a449732a935a43b0
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-11-17 10:45:08 -0300

    Validador de nome dos produtos

---
+++ apps/product/forms/validators/names_product_validator.py
@@ -0,0 +1,7 @@
+from re import match
+
+
+def name_product_validator(name: str, errors_list):
+    pattern = r'^[a-zA-ZáàãâÁÀÃÂéêÉÊóõôÓÕÔúÚ0-9\s-]{2,70}$'
+    if not match(pattern, str(name)):
+        errors_list['name'] = 'Name invalid, please review it'

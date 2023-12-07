b77d4248767ffa82e5e0181bf7e80f90e28fdcec
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-12-07 09:10:29 -0300

    Validando descrição dos produtos

--- apps/product/forms/product_form.py
+++ apps/product/forms/product_form.py
@@ -1,6 +1,10 @@
 from django.forms import ModelForm
 from product.models import Product
-from product.forms.validators import name_product_validator, brand_product_validator
+from product.forms.validators import (
+    name_product_validator,
+    brand_product_validator,
+    description_product_validator
+)
 
 
 class ProductModelForm(ModelForm):
@@ -12,9 +16,11 @@ class ProductModelForm(ModelForm):
     def clean(self):
         name = self.cleaned_data.get('name')
         brand = self.cleaned_data.get('brand')
+        description = self.cleaned_data.get('description')
         errors_list = {}
         name_product_validator(name, errors_list)
         brand_product_validator(brand, errors_list)
+        description_product_validator(description, errors_list)
         if errors_list:
             for error in errors_list:
                 error_message = errors_list[error]

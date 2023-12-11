f9140dcee50a80abc14310a7ca5bd7bd9ccef9b9
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-12-11 09:05:57 -0300

    Registrando o modelos admin de pagamentos

--- apps/client/admin/register.py
+++ apps/client/admin/register.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from client.models import Client, Account, Purchase
+from client.models import Client, Account, Purchase, Payment
 from django.contrib.auth.models import User, Group
-from client.admin.models_admin import ClientAdmin, AccountAdmin, PurchaseAdmin, UserAdmin, GroupAdmin, Manager
+from client.admin.models_admin import (
+    ClientAdmin,
+    AccountAdmin,
+    PurchaseAdmin,
+    PaymentAdmin,
+    UserAdmin,
+    GroupAdmin,
+    Manager
+)
 
 
 admin.site = Manager()
@@ -11,5 +19,6 @@ admin.site = Manager()
 admin.site.register(Client, ClientAdmin)
 admin.site.register(Account, AccountAdmin)
 admin.site.register(Purchase, PurchaseAdmin)
+admin.site.register(Payment, PaymentAdmin)
 admin.site.register(User, UserAdmin)
 admin.site.register(Group, GroupAdmin)

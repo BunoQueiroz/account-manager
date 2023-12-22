e46deabe12dc6395e7d9b8a416de5fde52b23488
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-12-22 07:59:15 -0300

    Removendo diretório de arquivos estáticos

--- config/settings.py
+++ config/settings.py
@@ -124,7 +124,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'nginx-config/static/')
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'style/')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

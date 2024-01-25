a6916fd91d296718b8b35cce86dee9d361825e74
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2024-01-25 09:03:56 -0300

    Corrigindo validação de nome e sobrenome de clientes

--- apps/client/forms/client_validators/names_validator.py
+++ apps/client/forms/client_validators/names_validator.py
@@ -1,17 +1,14 @@
 from re import match
 
 
-def name_invalid(first_name):
-    pattern = r'^[a-zA-Z\sâáãÂÁÃéêÉÊíîÍÎóôÓÔúÚñÑ]{2,30}$'
-    return False if match(pattern, first_name) else True
-
-
 def first_name_validator(first_name, errors_lits):
-    if name_invalid(first_name):
+    pattern = r'^[a-zA-Z\sâáãÂÁÃéêÉÊíîÍÎóôÓÔúÚñÑçÇ]{2,30}$'
+    if not match(pattern, first_name):
         errors_lits['first_name'] = 'First name invalid'
 
 
 def last_name_validator(last_name, erros_list):
-    if last_name and name_invalid(last_name):
+    pattern = r'^[a-zA-Z\sâáãÂÁÃéêÉÊíîÍÎóôÓÔúÚñÑçÇ]{2,70}$'
+    if last_name and not match(pattern, last_name):
         erros_list['last_name'] = 'Last name invalid'
 

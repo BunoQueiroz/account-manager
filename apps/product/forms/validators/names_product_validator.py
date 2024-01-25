b932c594ca393a5e67494cc077d72ad7d977041e
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2024-01-25 08:20:12 -0300

    Corrigindo erros de validação dos nomes de produtos

--- apps/product/forms/validators/names_product_validator.py
+++ apps/product/forms/validators/names_product_validator.py
@@ -2,6 +2,6 @@ from re import match
 
 
 def name_product_validator(name: str, errors_list):
-    pattern = r'^[a-zA-ZáàãâÁÀÃÂéêÉÊóõôÓÕÔúÚ0-9\s-]{2,70}$'
+    pattern = r'^[a-zA-ZáàãâÁÀÃÂéêÉÊóõôÓÕÔúÚçÇ0-9\s-]{2,70}$'
     if not match(pattern, str(name)):
         errors_list['name'] = 'Name invalid, please review it'

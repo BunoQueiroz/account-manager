a0cc5dc53ca73129bf1ced17e06fc3b9d3ccaaf9
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2024-02-01 15:13:28 -0300

    Alterando a forma como as variáveis de ambiente são pegas v1

--- config/settings_dev.py
+++ config/settings_dev.py
@@ -13,10 +13,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = os.environ('DEBUG', False)
 
 ALLOWED_HOSTS = ['*']
 

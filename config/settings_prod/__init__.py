bed785599b8f0347c6069a9deaca96b338ac3f29
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2024-02-22 09:14:49 -0300

    Modularizando as configurações de produção

---
+++ config/settings_prod/__init__.py
@@ -0,0 +1,13 @@
+from .main import *
+
+from .app_def import *
+
+from .password_validation import *
+
+from .data_base import *
+
+from .internalization import *
+
+from .static_files import *
+
+from .storage import *

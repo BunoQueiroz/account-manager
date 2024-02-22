6444346c6aa3a4f045c17c53a5c60f0bf0efcb78
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2024-02-22 09:15:50 -0300

    Configurações de banco de dados 'settings_prod'

---
+++ config/settings_prod/data_base.py
@@ -0,0 +1,27 @@
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
+    }
+}
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
